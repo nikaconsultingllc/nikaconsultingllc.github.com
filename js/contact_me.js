afccc3bea27e6ef219dd8f2692f08eea92aa135f
Alexey Solilin <solilin@jetrockets.ru>
2015-09-03 16:21:33 +0300

    added CNAME

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,70 +1,20 @@
-// $(function() {
+$(function() {
 
-//     $("input,textarea").jqBootstrapValidation({
-//         preventSubmit: true,
-//         submitError: function($form, event, errors) {
-//             // additional error messages or events
-//         },
-//         submitSuccess: function($form, event) {
-//             event.preventDefault(); // prevent default submit behaviour
-//             // get values from FORM
-//             var name = $("input#mce-NAME").val();
-//             var email = $("input#mce-EMAIL").val();
-//             var phone = $("input#mce-PHONE").val();
-//             var message = $("textarea#mce-MESSAGE").val();
-//             var firstName = name; // For Success/Failure Message
+    $("input,textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
 
-//             $.ajax({
-//                 url: "https://nikaconsultingllc.us11.list-manage.com/subscribe/post?u=459dd777b359fb92247c404cf&id=3b9be077e2",
-//                 crossDomain: true,
-//                 type: "POST",
-//                 data: {
-//                     name: name,
-//                     phone: phone,
-//                     email: email,
-//                     message: message
-//                 },
-//                 cache: false,
-//                 // dataType: 'jsonp',
-
-//                 success: function() {
-//                     // Success message
-//                     $('#success').html("<div class='alert alert-success'>");
-//                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-//                         .append("</button>");
-//                     $('#success > .alert-success')
-//                         .append("<strong>Your message has been sent. </strong>");
-//                     $('#success > .alert-success')
-//                         .append('</div>');
-
-//                     //clear all fields
-//                     $('#contactForm').trigger("reset");
-//                 },
-//                 error: function() {
-//                     // Fail message
-//                     $('#success').html("<div class='alert alert-danger'>");
-//                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-//                         .append("</button>");
-//                     $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-//                     $('#success > .alert-danger').append('</div>');
-//                     //clear all fields
-//                     $('#contactForm').trigger("reset");
-//                 },
-//             })
-//         },
-//         filter: function() {
-//             return $(this).is(":visible");
-//         },
-//     });
-
-//     $("a[data-toggle=\"tab\"]").click(function(e) {
-//         e.preventDefault();
-//         $(this).tab("show");
-//     });
-// });
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
 
 
-// /*When clicking on Full hide fail/success boxes */
-// $('#name').focus(function() {
-//     $('#success').html('');
-// });
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
+});
